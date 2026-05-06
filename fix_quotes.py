731292a99e0f6639dcb4579fb6aa30e41db39239
Nghia Tran <trongnghia23042005@gmail.com>
2026-05-06 14:30:56 +0700

    Cleanup project, organize scripts, and add comprehensive guide

--- fix_quotes.py
+++
@@ -1,4 +0,0 @@
-import re
-content = open(r'd:\lap_trinh_gis\Web_QLDD\myapp\templates\myapp\nguoi_dung\user_dashboard.html', encoding='utf-8').read()
-content = re.sub(r'default:\\"([^"]*)\\"', r"default:'\1'\", content)
-open(r'd:\lap_trinh_gis\Web_QLDD\myapp\templates\myapp\nguoi_dung\user_dashboard.html', 'w', encoding='utf-8').write(content)
